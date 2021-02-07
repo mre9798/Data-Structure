156e82d659896928679171070d061dd3f764373e
Eldhose Paul <77835549+mre9798@users.noreply.github.com>
2021-02-07 10:59:36 +0530

    Update lab2.3 set.c

--- lab2.3 set.c
+++ lab2.3 set.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
+
 int ar[10],a,b,i,j;
+
 void main()
 {
-    int ch;
-    create();
+    int x;
+    for(i=0;i<10;i++)
+    {
+        ar[i]=i;
+    }
     while(1)
     {
         printf("\n\n1.Union\n2.Find\n3.Display\n4.Exit");
         printf("\nEnter your choice:");
-        scanf("%d",&ch);
-        switch(ch)
+        scanf("%d",&x);
+        switch(x)
         {
-            case 1:unionop();
+            case 1:setunion();
             break;
-            case 2:find();
+            case 2:search();
             break;
             case 3:display();
             break;
@@ -22,13 +27,7 @@ void main()
         }
     }
 }
-void create()
-{
-    for(i=0;i<10;i++)
-    {
-        ar[i]=i;
-    }
-}
+
 void display()
 {
     printf("\n");
@@ -37,59 +36,54 @@ void display()
         printf("%d\t|",ar[i]);
     }
 }
-void unionop()
+
+void setunion()
 {
     int t,m;
     while(1)
-    {   a="";
-    b="";
-         printf("\nEnter the elements (a,b):");
-    scanf("%d,%d",&a,&b);
-    if(a==NULL|| b==NULL)
     {
-         printf("\nEnter the input as->a,b");
-
+        a="";
+        b="";
+        printf("\nEnter the elements (a,b):");
+        scanf("%d,%d",&a,&b);
+        if(a==NULL|| b==NULL)
+        {
+            printf("\nEnter the input as->a,b");
+        }
+        else if((a<10)&& (a>=0)&&(b<10)&& (b>=0))
+        {
+            break;
+        }
+        else
+            printf("Invalid input Please enter again\nEnter the input as->a,b");
     }
-
-    else if((a<10)&& (a>=0)&&(b<10)&& (b>=0))
+    int TEMP = ar[ a ];
+    for(int i = 0; i < 10;i++)
     {
-        break;
-    }
-
-    else
-        printf("Invalid input Please enter again\nEnter the input as->a,b");
-    }
-
-      int TEMP = ar[ a ];
-for(int i = 0; i < 10;i++)
-    {
-    if(ar[ i ] == TEMP)
-    ar[ i ] = ar[ b ];
+        if(ar[ i ] == TEMP)
+        ar[ i ] = ar[ b ];
     }
     display();
 }
-void find()
+
+void search()
 {
    while(1)
     {
-
          printf("\nEnter the elements (a,b):");
-    scanf("%d,%d",&a,&b);
-    if(a==NULL|| b==NULL)
-    {
-         printf("\nEnter the input as->a,b");
+         scanf("%d,%d",&a,&b);
+         if(a==NULL|| b==NULL)
+         {
+             printf("\nEnter the input as->a,b");
 
+         }
+         else if((a<10)&& (a>=0)&&(b<10)&& (b>=0))
+         {
+             break;
+         }
+         else
+             printf("Invalid input Please enter again\nEnter the input as->a,b");
     }
-
-    else if((a<10)&& (a>=0)&&(b<10)&& (b>=0))
-    {
-        break;
-    }
-
-    else
-        printf("Invalid input Please enter again\nEnter the input as->a,b");
-    }
-
     if(ar[ a ] == ar[ b ])
         printf("\nTrue");
     else
