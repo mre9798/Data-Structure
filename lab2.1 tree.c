3aeb5bd51620035a0517c9cc3b5d5da2ef3a7a1f
Eldhose Paul <77835549+mre9798@users.noreply.github.com>
2021-02-07 09:51:23 +0530

    Update lab2.1 tree.c

--- lab2.1 tree.c
+++ lab2.1 tree.c
@@ -1,95 +1,93 @@
- #include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
-struct bsnode
+
+struct tnode
 {
     int data;
-    struct bsnode *left;
-    struct bsnode *right;
+    struct tnode *left;
+    struct tnode *right;
 };
- int mn=0,n;
-struct bsnode *root=0;
-struct bsnode *delet(struct bsnode *,int);
+
+int mn=0,n;
+struct tnode *root=0;
+struct tnode *delet(struct tnode *,int);
 struct node* search(struct node *,int);
 void create();
-void inorder(struct bsnode *ptr1);
-void postorder(struct bsnode *ptr1);
-void preorder(struct bsnode *ptr1);
+void inord(struct tnode *ptr1);
+void postord(struct tnode *ptr1);
+void preord(struct tnode *ptr1);
+
 void main()
 {
-
     int ch,j,value;
-
     while (1)
     {
-        printf("\n 1 -Insertion");
-   printf("\n 2 -Deletion");
-   printf("\n 3 -Inorder Traversal");
-    printf("\n 4 -Postorder Traversal");
-     printf("\n 5 -Preorder Traversal");
-         printf("\n 6- Search");
-    printf("\n 7- Exit");
-        printf("\n Enter choice : ");
-        scanf("%d", &ch);
+         printf("\n 1 -Insertion\n 2 -Deletion");
+         printf("\n 3 -Inorder Traversal\n 4 -Postorder Traversal\n 5 -Preorder Traversal");
+         printf("\n 6- Search\n 7- Exit"");
+         printf("\n Enter choice : ");
+         scanf("%d", &ch);
 
         switch (ch)
         {
-        case 1:
-            create();
-            break;
-        case 2:
-            inorder(root);
-            printf("\nEnter the element to be deleted:");
-            scanf("%d",&n);
-            j=search(root,n);
-            if(j==1)
-            {
-                root=delet(root,n);
-                printf("\n The tree after deletion\n");
-                inorder(root);
-            }
-            else
-            {
-                printf("\nElement is not present");
-            }
-
-            break;
-        case 3:inorder(root);
-            break;
-        case 4:postorder(root);
-            break;
-        case 5:preorder(root);
-            break;
-        case 6: printf("\nEnter the element:");
-                scanf("%d",&value);
-            j=search(root,value);
-            if(j==1)
-            {
-                printf("\nElement is present");
-            }
-
-            else
-            {
-                printf("\nElement is not present");
-            }
-
-            break;
-        case 7:
-            exit(0);
-
-        default :
-            printf(" Wrong choice, Please enter correct choice  ");
-            break;
+             case 1:
+                  create();
+                  break;
+             case 2:
+                  inord(root);
+                  printf("\nEnter the element to be deleted:");
+                  scanf("%d",&n);
+                  j=search(root,n);
+                  if(j==1)
+                  {
+                       root=delet(root,n);
+                       printf("\n The tree after deletion\n");
+                       inord(root);
+                  }
+                  else
+                  {
+                       printf("\nElement is not present");
+                  }
+                  break;
+             case 3:
+                  inord(root);
+                  break;
+             case 4:
+                  postord(root);
+                  break;
+             case 5:
+                  preord(root);
+                  break;
+             case 6:
+                  printf("\nEnter the element:");
+                  scanf("%d",&value);
+                  j=search(root,value);
+                  if(j==1)
+                  {
+                       printf("\nElement is present");
+                  }
+                  else
+                  {
+                       printf("\nElement is not present");
+                  }
+                  break;
+             case 7:
+                  exit(0);
+             default :
+                  printf(" Wrong choice, Please enter correct choice  ");
+                  break;
         }
     }
 }
+                
 void create()
 {
     int value;
-    struct bsnode *ptr;
-    struct bsnode *parent;
+    struct tnode *ptr;
+    struct tnode *parent;
     printf("\nEnter the value:");
     scanf("%d",&value);
-    ptr=(struct bsnode *)malloc(1*sizeof(struct bsnode));
+    ptr=(struct tnode *)malloc(1*sizeof(struct tnode));
     ptr->data=value;
     ptr->left=0;
     ptr->right=0;
@@ -98,7 +96,7 @@ void create()
         root=ptr;
     else
     {
-        struct bsnode *temp;
+        struct tnode *temp;
         temp=root;
         while(temp!=0)
         {
@@ -106,173 +104,139 @@ void create()
             mn++;
             if(temp->data>value)
             {
-                     temp=temp->left;
-
+                 temp=temp->left;
             }
             else
             {
-             temp=temp->right;
-
+                 temp=temp->right;
             }
         }
-
         if(value<parent->data)
         {
             parent->left=ptr;
-
         }
-
         else
         {
             parent->right=ptr;
-
         }
-
     }
 }
-void inorder(struct bsnode *ptr1)
+                
+void inord(struct tnode *ptr1)
 {
-        if (root == NULL)
-
+    if (root == NULL)
     {
-
-        printf("No elements in a tree to display");
-
-        return;
-
+         printf("No elements in a tree to display");
+         return;
     }
-
     if (ptr1->left != NULL)
-
-        inorder(ptr1->left);
-
+         inorder(ptr1->left);
     printf("%d -> ", ptr1->data);
-
     if (ptr1->right != NULL)
-
-        inorder(ptr1->right);
-
+         inord(ptr1->right);
 }
-void postorder(struct bsnode *ptr1)
+                
+void postord(struct tnode *ptr1)
 {
-        if (root == NULL)
+    if (root == NULL)
     {
-        printf("No elements in a tree to display");
-
-        return;
+         printf("No elements in a tree to display");
+         return;
     }
-
     if (ptr1->left != NULL)
-        inorder(ptr1->left);
-
+         inord(ptr1->left);
     if (ptr1->right != NULL)
-        inorder(ptr1->right);
-
+         inord(ptr1->right);
     printf("%d -> ", ptr1->data);
 }
-void preorder(struct bsnode *ptr1)
+                
+void preord(struct tnode *ptr1)
 {
-        if (root == NULL)
+    if (root == NULL)
     {
-        printf("No elements in a tree to display");
-
-        return;
+         printf("No elements in a tree to display");
+         return;
     }
     printf("%d -> ", ptr1->data);
-
     if (ptr1->left != NULL)
-        inorder(ptr1->left);
-
+         inord(ptr1->left);
     if (ptr1->right != NULL)
-        inorder(ptr1->right);
+         inord(ptr1->right);
  }
- struct bsnode* minNode(struct bsnode *root) {
+                
+struct tnode* minNode(struct tnode *root)
+{
     if (root->left != NULL)
         return minNode(root->left);
     else
         return root;
 }
- struct bsnode* delet(struct bsnode *ptr, int value) {
-    if(ptr == NULL){
-          return NULL;
-     }
-    else {
-        if(value < ptr->data)
-            ptr->left = delet(ptr->left, value);
-
-        else if(value > ptr->data)
-            ptr->right = delet(ptr->right, value);
-
-
-        else {
-
-            if(ptr->left == NULL && ptr->right == NULL)
-                ptr = NULL;
-
-
-            else if(ptr->left == NULL) {
-                ptr = ptr->right;
-            }
-
-
-            else if(ptr->right == NULL) {
-                ptr= ptr->left;
-            }
-
-
-            else {
-                struct bsnode *temp = minNode(ptr->right);
-
-                ptr->data = temp->data;
-
-                ptr->right = delet(ptr->right, temp->data);
-            }
-        }
-        return ptr;
-    }
-}
-
-struct node* search(struct node *ptr,int value)
+                
+struct tnode* delet(struct tnode *ptr, int value)
 {
-    int f=0;
-
-     if(root==0)
-        printf("\nTree empty");
-     else if(root->data==value)
-            f=1;
+    if(ptr == NULL)
+    {
+         return NULL;
+    }
     else
     {
-        struct bsnode *temp;
-        temp=root;
-        while(temp!=0)
-        {
-            if(temp->data>value)
-            {
-                if(temp->data==value)
-                {
-
-                     f=1;
-                     break;
-                }
-                else
-                     temp=temp->left;
-
-            }
-            else
-            {
-
-                if(temp->data==value)
-                {
-
-                     f=1;
-                     break;
-                }
-                else
-             temp=temp->right;
-
-            }
-        }
+         if(value < ptr->data)
+              ptr->left = delet(ptr->left, value);
+         else if(value > ptr->data)
+              ptr->right = delet(ptr->right, value);
+         else
+         {
+              if(ptr->left == NULL && ptr->right == NULL)
+                   ptr = NULL;
+              else if(ptr->left == NULL)                        
+                   ptr = ptr->right;                     
+              else if(ptr->right == NULL) 
+                   ptr= ptr->left;            
+              else
+              {
+                   struct tnode *temp = minNode(ptr->right);
+                   ptr->data = temp->data;
+                   ptr->right = delet(ptr->right, temp->data);
+              }
+         }     
+         return ptr;
+     }
 }
-
-return f;
+                
+struct node* search(struct node *ptr,int value)
+{
+     int f=0;
+     if(root==0)
+          printf("\nTree empty");
+     else if(root->data==value)
+          f=1;
+     else
+     {
+          struct tnode *temp;
+          temp=root;
+          while(temp!=0)
+          {
+               if(temp->data>value)
+               {
+                    if(temp->data==value)
+                    {
+                         f=1;
+                         break;
+                    }
+                    else
+                         temp=temp->left;
+               }
+               else
+               {
+                    if(temp->data==value)
+                    {
+                         f=1;
+                         break;
+                    }
+                    else
+                         temp=temp->right;
+               }
+          } 
+     }
+     return f;
 }
